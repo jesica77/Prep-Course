e61238cade1362259527ceb4d5368b8a81e18b98
jesica77 <moralesjesica77@gmail.com>
2022-07-31 15:29:31 -0300

    boleana

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "hola";
 const nuevoNum = 123;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = verdadero;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
